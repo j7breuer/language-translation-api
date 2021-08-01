05214e3faaae41b4cc5134f578f33b05a845f1d8
j7breuer <66193217+j7breuer@users.noreply.github.com>
2021-08-01 00:47:59 -0400

    Updating new functions in translation script

--- app/huggingface_translation.py
+++ app/huggingface_translation.py
@@ -15,7 +15,7 @@ class LanguageTranslation:
         }
         self.models = {}
     
-    def load_tokenizer(from_lang: str, to_lang: str):
+    def load_tokenizer(self, from_lang: str, to_lang: str):
         '''
         desc: 
             Given two languages to translate from and to, return the Helsinki tokenizer.
@@ -26,10 +26,11 @@ class LanguageTranslation:
         oupt:
             [tokenizer]: AutoTokenizer object specific to language combination.
         '''
+        # Create model string prefix for download/loading, uses helsinki model name
         model_string_name = f"{self.model_prefix_name}{from_lang}-{to_lang}"
         return AutoTokenizer.from_pretrained(model_string_name)
 
-    def load_model(from_lang: str, to_lang: str):
+    def load_model(self, from_lang: str, to_lang: str):
         '''
         desc: 
             Given two languages to translate from and to, return the Helsinki model.
@@ -40,10 +41,11 @@ class LanguageTranslation:
         oupt:
             [model]: AutoModel object specific to language combination.
         '''
+        # Create model string name for download/loading, uses helsinki model name
         model_string_name = f"{self.model_prefix_name}{from_lang}-{to_lang}"
         return AutoModelForSeq2SeqLM.from_pretrained(model_string_name).to(self.gpu)
 
-    def load_languages(lang_list: list):
+    def load_languages(self, lang_list: list):
         '''
         desc:
             Given list of languages to be able to translate, load the tokenizer/models
@@ -55,39 +57,85 @@ class LanguageTranslation:
             self.models [dict]: updated with tokenizer/model combos from lang_list.
         '''
         all_models = {}
+        # Iterate through languages supported to initiate all models/tokenizers
         for k,v in self.languages_supported.iteritems():
+            # Skip english
             if k != "English":
-                self.models[f"{k}-en"]["tokenizer"] = load_tokenizer(k, "en")
-                self.models[f"{k}-en"]["model"] = load_model(k, "en")
+                # Store in class so it can be referenced internally
+                self.models[f"{k}-en"]["tokenizer"] = self.load_tokenizer(k, "en")
+                self.models[f"{k}-en"]["model"] = self.load_model(k, "en")
 
-    def translate_single(from_lang: str, to_lang: str, text: list) -> list:
+    def translate_single(self, from_lang: str, to_lang: str, text: list) -> list:
         '''
+        desc:
+            Given a from_lang, to_lang, and text, translated the text using transformer models.
+            Runs very slow on CPU, is configured to run on GPU.
+        inpt:
+            from_lang [str]: 2 letter abbreviation of langauge to translated from.
+            to_lang [str]: 2 letter abbreviation of language to translate to.
+            text [list]: list of strings to translate.
         '''
-        tokenized_text = self.models[f"{from_lang}-{to_lang}"]["tokenizer"].prepare_seq2seq_batch([text], return_tensors = "pt").to(self.gpu)
+        # Tokenize the text using tokenizer stored in class
+        tokenized_text = self.models[f"{from_lang}-{to_lang}"]["tokenizer"].prepare_seq2seq_batch(text, return_tensors = "pt").to(self.gpu)
+        # Generate translated tokens using model stored in class
         translated_tokens = self.models[f"{from_lang}-{to_lang}"]["model"].generate(**tokenized_text)
+        # Convert translated tokens to english text using tokenizer stored in class
         translated_text = self.models[f"{from_lang}-{to_lang}"]["tokenizer"].batch_decode(translated_tokens, skip_special_tokens = True)
         torch.cuda.empty_cache()
         return translated_text
 
+    def deconstruct_inpt(self, inpt_list: list) -> list:
+        '''
+        desc:
+            Given an inpt list of dicts, add a placement k,v pair starting from 0.
+        inpt:
+            inpt_list [list]: list of dicts to add the new placement k,v pair.
+        oupt:
+            [list]: list of dicts with new k,v pair.
+        '''
+        # Add placement key,value pair to dictionary
+        return [dict(item, **{"placement": count}) for count, item in enumerate(inpt_list, 0)]
+
+    def reconstruct_inpt(self, inpt_list: list) -> list:
+        '''
+        desc:
+            Given an inpt list of dicts, sort the order on the placement key.
+            This ensures that the API's response is in the same order it was received.
+        inpt:
+            inpt_list [list]: list of dicts that have a k,v pair for 'placement'.
+        oupt:
+            [list]: sorted list of dictionaries on the placement key.
+        '''
+        # Reorder based on placement key,value pair in dictionary
+        return sorted(inpt_list, key = lambda k: k["placement"])
+
     def translate_batch(inpt_list: list):
         '''
+        desc:
+            Given a list of dictionaries with from_lang and text, translated in batch.
+        inpt:
+            inpt_list [list]: list of dicts that have the k,v pairs:
+                - from_lang [str]: 2 letter abbreviation of language.
+                - text [str]: text to be translated.
+        oupt:
+            oupt_list [list]: inpt_list where each dict has a new k,v pair of translated text.
         '''
         # Create a list of all foreign languages to translate from into english
+        inpt_list = self.deconstruct_inpt(inpt_list)
         foreign_lang_list = list(set([x["from_lang"] for x in inpt_list]))
-        # Organize dictionary such that all text in each language are grouped together
-        # This will speed up translations by reducing redundancy
-        inpt_dict = {}
+        # Loop through languages and translate in batches
+        oupt_list = []
         for lang in foreign_lang_list:
-            translated_text = self.translate_single(lang, "en", [x["text"] for x in inpt_list if x["from_lang"] == lang])
-            counter = 0
-            [dict(item, **{"translated_text": translated_text[count]} for count, item in enumerate(inpt_list, 0) if item["from_lang"] == lang]
-            for x in inpt_list:
-                x["translated_text"] = translated_text[counter]
-                counter += 1
-        for lang, text_list_dict in inpt_dict.iteritems():
-            text_list = [x["text"] for x in text_list]
-            [inpt_dict[lang] for x in translate_single(lang, "en", text_list)]
-        return oupt
+            # Pull all dicts from certain langauge
+            cur_lang_inpt_list = [x for x in inpt_list if x["from_lang"] == lang]
+            # Translate text in batch
+            translated_text = self.translate_single(lang, "en", [x["text"] for x in cur_lang_inpt_list])
+            cur_lang_inpt_list = [dict(item, **{"translated_text": translated_text[count]}) for count, item in enumerate(translated_text, 0)]
+            # Append list of dicts to oupt list
+            oupt_list = oupt_list + cur_lang_inpt_list
+        # Reorder the list of dicts
+        oupt_list = self.reconstruct_inpt(oupt_list)
+        return oupt_list
             
 
 
