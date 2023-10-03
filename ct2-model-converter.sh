409d341c3f5dca0ae9d99ec1e36e5d95c3b1e7db
j7breuer <jmbreuer@vt.edu>
2023-10-03 00:39:09 -0400

    Updating path for ct2 transformers

--- ct2-model-converter.sh
+++ ct2-model-converter.sh
@@ -2,5 +2,5 @@
 
 # Loop through keys in the JSON and download the models and convert, storing in ./app/models
 jq -r 'keys[]' "$1" | while read -r key; do
-  ct2-transformers-converter --model Helsinki-NLP-opus-mt-$key-en --output_dir ./app/models/$key_en
+  /var/lib/jenkins/.local/bin/ct2-transformers-converter --model Helsinki-NLP-opus-mt-$key-en --output_dir ./app/models/$key_en
 done
