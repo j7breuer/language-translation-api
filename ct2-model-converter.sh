e85ad39d1824ebddfc13d48f25fd39df9aacc747
j7breuer <jmbreuer@vt.edu>
2023-10-03 22:05:24 -0400

    Final bug fix

--- ct2-model-converter.sh
+++ ct2-model-converter.sh
@@ -3,9 +3,11 @@
 # Loop through keys in the JSON and download the models and convert, storing in ./app/models
 jq -r 'keys[]' "$1" | while read -r key; do
   # If key is 'en' then skip
-  if ["$key" == "en"]; then
+  if [ "${key}" == "en" ]; then
     continue
   fi
+
   # Convert models
   ct2-transformers-converter --model Helsinki-NLP/opus-mt-${key}-en --output_dir ./app/models/${key}_en --force
+
 done
