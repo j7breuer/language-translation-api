32f71fb36ce48f522f22c497d149847e74164a9c
j7breuer <jmbreuer@vt.edu>
2023-10-05 22:55:35 -0400

    Adding force download for models

--- ct2-model-converter.sh
+++ ct2-model-converter.sh
@@ -9,10 +9,10 @@ jq -r 'keys[]' "$1" | while read -r key; do
 
   # Convert models from english to language
   echo "Downloading and converting: ${key}-en..."
-  ct2-transformers-converter --model Helsinki-NLP/opus-mt-${key}-en --output_dir ./models/${key}_en
+  ct2-transformers-converter --model Helsinki-NLP/opus-mt-${key}-en --output_dir ./models/${key}_en --force
 
   # Convert models from language to english
   echo "Downloading and converting: en-${key}..."
-  ct2-transformers-converter --model Helsinki-NLP/opus-mt-en-${key} --output_dir ./models/en_${key}
+  ct2-transformers-converter --model Helsinki-NLP/opus-mt-en-${key} --output_dir ./models/en_${key} --force
 
 done
